c513176dd8295eb12d3dda4352799c1a3ef1542c
Winakki <124130956+Winakki@users.noreply.github.com>
2023-02-21 19:12:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1.9";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            double a = Convert.ToDouble(str,numberFormatInfo);
+
+            Console.WriteLine(a);
+        }
+    }
+}
