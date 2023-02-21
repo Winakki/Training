042e15fbd5567d035e021e48dff42d7e6be5619a
Winakki <124130956+Winakki@users.noreply.github.com>
2023-02-21 19:14:23 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp2
 
             NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
             {
-                NumberDecimalSeparator = ".",
+                NumberDecimalSeparator = ".", //Дает нам конвертировать дробное число т.к "." он не видит, а видит только ",". 
             };
 
             double a = Convert.ToDouble(str,numberFormatInfo);
