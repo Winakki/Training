3f7f009071fcf5da6c58b1580170d385095ba0df
Winakki <124130956+Winakki@users.noreply.github.com>
2023-02-22 21:57:10 +0500

    Add files via upload

---
+++ Конвертер валюты.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double RUB;
+            double USD = 74.71;
+            Console.WriteLine("Введите сумму перевода из RUB в USD");
+            RUB = double.Parse(Console.ReadLine());
+            
+
+            double result_USD = (RUB / USD);
+
+            Console.WriteLine(RUB + " RUB в USD = "+ Math.Round(result_USD,2));
+        }
+    }
+}
