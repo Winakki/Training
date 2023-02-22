0269ff914ace25380570a866fc2f19ce3a087faf
Winakki <124130956+Winakki@users.noreply.github.com>
2023-02-22 21:10:22 +0500

    Add files via upload

---
+++ Конвертация.Сложение.Произведение.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string str;
+            int a;int b;int c;
+
+            Console.WriteLine("Введите 1 число");
+             str = Console.ReadLine();
+             a = Convert.ToInt32(str);
+
+            Console.WriteLine("Введите 2 число");
+            str = Console.ReadLine(); 
+            b = Convert.ToInt32(str);
+
+            Console.WriteLine("Введите 3 число");
+            str = Console.ReadLine();
+            c = Convert.ToInt32(str);
+
+            int result1 = a + b + c;
+            int result2 = a * b * c;
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        
+        }
+    }
+}
